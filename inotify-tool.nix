741b1e6c01c6d5b4946f0723dad2b103a71b9234
Gareth Charnock <gareth.charnock@gmail.com>
2018-10-27 15:18:57 +0100

    Set up some testing infastructure.

--- inotify-tool.nix
+++ inotify-tool.nix
@@ -1,8 +1,8 @@
 { mkDerivation, aeson, base, binary, bytestring, cryptonite
-, directory, filepath, hashable, hashtables, hinotify, interpolate
-, memory, mtl, network, optparse-applicative, pipes, pipes-binary
-, pipes-bytestring, pipes-parse, rawfilepath, stdenv, text
-, transformers, unix, unliftio, unliftio-core, utf8-string
+, directory, filepath, hashable, hashtables, hinotify, hspec
+, interpolate, memory, mtl, network, optparse-applicative, pipes
+, pipes-binary, pipes-bytestring, pipes-parse, rawfilepath, stdenv
+, text, transformers, unix, unliftio, unliftio-core, utf8-string
 }:
 mkDerivation {
   pname = "inotify-tool";
@@ -10,13 +10,18 @@ mkDerivation {
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ aeson base bytestring text ];
-  executableHaskellDepends = [
+  libraryHaskellDepends = [
     aeson base binary bytestring cryptonite directory filepath hashable
-    hashtables hinotify interpolate memory mtl network
-    optparse-applicative pipes pipes-binary pipes-bytestring
-    pipes-parse rawfilepath text transformers unix unliftio
-    unliftio-core utf8-string
+    hashtables hinotify interpolate memory mtl network pipes
+    pipes-binary pipes-bytestring pipes-parse rawfilepath text
+    transformers unix unliftio unliftio-core utf8-string
+  ];
+  executableHaskellDepends = [
+    aeson base binary bytestring network optparse-applicative text unix
+    unliftio unliftio-core
+  ];
+  testHaskellDepends = [
+    base bytestring hspec rawfilepath text unix
   ];
   license = stdenv.lib.licenses.unfree;
   hydraPlatforms = stdenv.lib.platforms.none;
--- share/LibWormhole.hs
+++ share/LibWormhole.hs
@@ -1,9 +1,15 @@
 
 module LibWormhole where
 
-import qualified Data.Text as T
-import Data.Aeson
-import GHC.Generics
+import qualified Data.Text                     as T
+import           Control.Monad.IO.Class
+import           Data.Aeson
+import           GHC.Generics
+import           Data.Monoid
+import           Data.ByteString               as BS
+import           System.Posix.ByteString.FilePath
+import           System.Posix.Directory.ByteString
+import Data.Word
 
 data Cmd = TreeCmd | DumpCmd deriving (Show, Generic)
 
@@ -17,3 +23,22 @@ data ClientMsg = ClientMsg {
 
 instance FromJSON ClientMsg
 instance ToJSON ClientMsg
+
+makePathAbsolute :: MonadIO m => RawFilePath -> m RawFilePath
+makePathAbsolute filepath = do
+    if BS.take 1 filepath == "/"
+        then return filepath
+        else do
+            let (first, rest) = BS.splitAt 3 filepath
+            if first == "../"
+                then makePathAbsolute rest
+                else do
+                    cwd <- liftIO $ getWorkingDirectory
+                    return $ cwd <> "/" <> filepath
+
+pathSeperator :: Word8
+pathSeperator = 47
+
+splitFilepath :: RawFilePath -> (RawFilePath, RawFilePath)
+splitFilepath filepath = (BS.init dirpath, filename)
+    where (dirpath, filename) = breakEnd (==pathSeperator) filepath
